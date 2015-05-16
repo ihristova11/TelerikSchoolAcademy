50d7fbe079eabae124937b427e34dd7e65c286b2
ihristova11 <hristova119@gmail.com>
2015-05-16 21:38:43 +0300

    Homework

---
+++ C# Part1/01. Introduction-to-Programming/2. Primitive-Data-Types-and-Variables/11. ExchangeValues/ExchangeValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ExchangeValues
+{
+    static void Main()
+    {
+        int firstVariable = 10;
+        int secondVariable = 5;
+        int thirdVariable = firstVariable;
+        Console.WriteLine(firstVariable + " " + secondVariable);
+        firstVariable = secondVariable;
+        secondVariable = thirdVariable;
+        Console.WriteLine(firstVariable + " " + secondVariable);
+
+    }
+}
+
---
+++ C# Part1/01. Introduction-to-Programming/7. HelloWorldProgram/HelloWorldProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HelloWorldProgram
+{
+    static void Main()
+    {
+        string greetingVariable = "Hello";
+        string locationVariable = "World";
+        object fullGreating = greetingVariable + " " + locationVariable + "!";
+        string fullGreatingString = (string)fullGreating;
+        Console.WriteLine(fullGreatingString);
+    }
+}
